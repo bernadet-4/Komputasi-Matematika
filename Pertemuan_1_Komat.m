93541541898f7a9fd1b68545c3c4bb97c27f938b
bernadet-4 <berthaangelic4@gmail.com>
2026-01-22 14:57:10 +0700

    Add files via upload

---
+++ Pertemuan_1_Komat.m
@@ -0,0 +1,8 @@
+%Menghitung Phytagoras
+clc 
+clear
+
+a = 3;
+b = 4;
+c = a^2 + b^2;
+disp(c)
---
+++ Pertemuan_2_Komat.m
@@ -0,0 +1,73 @@
+% Pertemuan 2 Komputasi Matematika 
+clc
+clear
+
+% n = input ('nilai : ');
+% if n > 70
+%     disp('anda lulus');
+% end
+% disp('coba lagi');   
+
+% n = input ('nilai : ');
+% if n > 70
+%     disp('anda lulus');
+% else
+%     disp('coba lagi');
+% end
+
+% n = input ('tinggi : ');
+% if n > 190
+%     disp('sangat tinggi');
+% elseif n > 160 && n < 190
+%     disp('cukup tinggi');
+% elseif n < 160
+%     disp('pendek');
+% else
+%     disp('mini');
+% end
+
+% hitungan = input ('operasi :');
+% a = 2;
+% b = 3;
+% switch hitungan 
+%     case 1
+%         y = a+b
+%     case 2
+%         y = a-b
+%     case 3
+%         y = a*b
+%     otherwise 
+%     disp('bukan pilihan');
+% end
+
+% %for i contoh 1
+% for i = 1:100
+%     disp(i);
+% end
+
+
+
+% %for i contoh 2 
+% a = 0;
+% for i = 1:5
+%     a = a+i
+% end
+% disp(a)
+
+% a = 0;
+% for i = 1:5
+%     if i == 2
+%         continue
+%     else 
+%         a = a+i
+%     end
+% end
+% disp(a)
+
+for i = 1:10
+    if i == 5
+        break
+    else 
+        disp(i)
+    end
+end
